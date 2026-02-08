2d5b41e1b8c9c9ef23dcbd82331231a460ff56d0
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2026-02-08 10:08:34 +0530

    added auth middleware

--- backend/src/http/routes/auth.route.ts
+++ backend/src/http/routes/auth.route.ts
@@ -1,10 +1,13 @@
-import express from 'express';
-import { signInHandler, signUpHandler } from '../controller/auth.controller.js';
+import express, { type Request, type Response } from 'express';
+import { getUserByIdHandler, signInHandler, signUpHandler } from '../controller/auth.controller.js';
+import { authRequest } from '../middleware/auth.middleware.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup',signUpHandler);
 
 authRouter.post('/signin',signInHandler);
+
+authRouter.get('/:id',authRequest,getUserByIdHandler);
 
 export default authRouter
